e4bc521f6f225c0cce0ec90a3e0a013532b7c7ba
mortezar68 <morteza.rezaei68@gmail.com>
2022-04-11 13:22:42 +0430

    FIXED:change dotnet version

--- SwaggerExampleAttrLib/EnumerableExtensions.cs
+++ SwaggerExampleAttrLib/EnumerableExtensions.cs
@@ -1,33 +1,34 @@
-namespace SwaggerExampleAttrLib;
-
-public static class EnumerableExtensions
+namespace SwaggerExampleAttrLib
 {
-    public static T? ConvertToObj<T>(this object? input) where T : new()
+    public static class EnumerableExtensions
     {
-        var result = new T();
-
-        if (input == null)
-            return default(T);
-
-        var allProps = input.GetType().GetProperties();
-        var allResultObjProps = result.GetType().GetProperties();
-        foreach (var prop in allProps)
+        public static T? ConvertToObj<T>(this object? input) where T : new()
         {
-            var foundTargetProp = allResultObjProps.FirstOrDefault(t => t.Name == prop.Name);
-            if (foundTargetProp == null) continue;
-            var valueTarget = prop.GetValue(input);
-            if (valueTarget == null) continue;
-            if (foundTargetProp.GetValue(result) != null &&
-                foundTargetProp.GetValue(result)?.GetType() == typeof(DateTime))
-            {
-                //foundTargetProp.SetValue(result, ValueTarget.ToEnDate());
-            }
-            else
-            {
-                foundTargetProp.SetValue(result, valueTarget);
-            }
-        }
+            var result = new T();
 
-        return result;
+            if (input == null)
+                return default(T);
+
+            var allProps = input.GetType().GetProperties();
+            var allResultObjProps = result.GetType().GetProperties();
+            foreach (var prop in allProps)
+            {
+                var foundTargetProp = allResultObjProps.FirstOrDefault(t => t.Name == prop.Name);
+                if (foundTargetProp == null) continue;
+                var valueTarget = prop.GetValue(input);
+                if (valueTarget == null) continue;
+                if (foundTargetProp.GetValue(result) != null &&
+                    foundTargetProp.GetValue(result)?.GetType() == typeof(DateTime))
+                {
+                    //foundTargetProp.SetValue(result, ValueTarget.ToEnDate());
+                }
+                else
+                {
+                    foundTargetProp.SetValue(result, valueTarget);
+                }
+            }
+
+            return result;
+        }
     }
 }
